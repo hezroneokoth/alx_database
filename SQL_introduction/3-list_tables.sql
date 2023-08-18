4fad177d5e49a49e6ab128ce1bcc065c686435e7
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-18 10:44:31 +0300

    Script that creates a table in current database

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- lists all the tables of a database in my MySQL server
 -- the database name will be passed as a MySQL command arg
-mysql -u username -p -e "USE your_database_name; SHOW TABLES;"
+mysql -u username -p -e "USE barding; SHOW TABLES;"
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,1 +1,3 @@
-SHOW DATABASES;
+-- creates a table called first_table in the current database
+-- database name is passed as a mysql command arg
+mysql -u username -p -e "USE barding; CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));"
