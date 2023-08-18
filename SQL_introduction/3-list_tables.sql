a4f866cf96cafe3327361af6323448ac0206291c
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-18 09:58:38 +0300

    Script that lists all database tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,1 +1,3 @@
-SHOW DATABASES;
+-- lists all the tables of a database in my MySQL server
+-- the database name will be passed as a MySQL command arg
+mysql -u username -p -e "USE your_database_name; SHOW TABLES;"
