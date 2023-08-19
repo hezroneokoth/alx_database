ee9184b977c56ac91ae6016f9c964a9beee2f815
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-19 13:30:41 +0300

    Displays no of records with ID=89 in table of DB

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,1 +1,3 @@
-SHOW DATABASES;
+-- displays the no of records with id = 89 in the first_table of the DB hbtn_0c_0
+-- The DB name will be passed as an arg of the mysql command
+SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
