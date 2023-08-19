305f53ea0034e679aa5d81d3e90dd1e851fae9e1
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-19 12:53:36 +0300

    Script that creates a table in current database

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- creates a table called first_table in the current database
 -- database name will be passed as a mysql command arg
-hbtn_0c_0;
+hbtn_test_db;
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
