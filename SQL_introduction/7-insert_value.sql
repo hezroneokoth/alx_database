51bc2b5a9cda2f04710fe01af7552ab5601f4953
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-19 13:35:09 +0300

    Script inserts new row

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,1 +1,3 @@
-SHOW DATABASES;
+-- inserts new row in the first_table in hbtn_0c_0 DB
+-- New row: id = 89 name = Holberton School
+INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Holberton School');
