6b1ccd13862b8c1d62168a8d987bc42985d0d607
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-19 13:10:17 +0300

    Script prints full desc of a table from a given DB

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,1 +1,6 @@
-SHOW DATABASES;
+-- prints the full description of the table first_table from the database hbtn_0c_0
+-- database name will be passed as an argument of the mysql command
+SELECT TABLE_NAME, CREATE_TABLE
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+AND TABLE_NAME = 'first_table';
