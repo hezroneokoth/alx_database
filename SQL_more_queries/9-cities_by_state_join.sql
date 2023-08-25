7229689da2f6ac86796c43082fce23e0168c3096
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-25 15:36:54 +0300

    Task9 lists all cities w id, name, state ascending

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities with their IDs, names & respective state names
-SELECT CONCAT(cities.id, ' - ', cities.name, ' - ', states.name) AS city_info
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
