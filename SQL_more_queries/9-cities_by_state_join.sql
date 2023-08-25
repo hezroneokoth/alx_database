fc69118584c291c9ed5434a5b3fe980bd2be7223
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-25 15:46:51 +0300

    Task 9 fixes how the cities name is displayed

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities with their IDs, names & respective state names
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
