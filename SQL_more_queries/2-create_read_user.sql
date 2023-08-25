d1a85e8447bc2817855470d180bbd9f98aa1cbd5
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-25 09:55:11 +0300

    Task 2 creates db & user, grants select privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates the hbtn_0d_2 db if it doesn't exist
+-- creates MySQL server user user_0d_2 if it doesn't exist
+-- then grants SELECT privilege on hbtn_0d_2 db to user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
