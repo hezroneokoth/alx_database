81e1838298f2cfdd5e85288c8b42d1677bdbdf21
Hezrone Okoth <hezzyokoth@gmail.com>
2023-11-03 15:20:25 +0300

    Task 5: Prints full description of table from DB

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0
 -- database name will be passed as an argument of the mysql command
 
-SELECT column_name, column_type, is_nullable, column_default
+SELECT CONCAT(table_name, column_name, column_type, is_nullable, column_default, ',')
 FROM information_schema.columns
-WHERE table_schema='$1' AND table_name='first_table';
+WHERE table_schema='hbtn_0c_0' AND table_name='first_table';
