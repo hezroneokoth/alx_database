976e13f3e3191d09cb09e761d976de0edbd7800a
Hezrone Okoth <hezzyokoth@gmail.com>
2023-11-03 15:15:41 +0300

    Task 5: Prints full description of table from DB

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
-#!/bin/bash
-
 -- prints the full description of the table first_table from the database hbtn_0c_0
 -- database name will be passed as an argument of the mysql command
 
-cur.execute("SHOW CREATE TABLE first_table")
+SELECT column_name, column_type, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_schema='$1' AND table_name='first_table';
